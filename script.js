49c910d5ba4a4e41e7242417059132c0aa91bf56
Sahil Kumar <sahilkumar7325@gmail.com>
2024-07-04 21:55:05 +0530

    First commit for tut

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+    const showSideBar = document.querySelector('.navbar-mobile')
+    showSideBar.style.display="block"
+}
+
+function hideSideBar() {
+    const hideSideBar = document.querySelector('.navbar-mobile')
+    hideSideBar.style.display="none"
+}
